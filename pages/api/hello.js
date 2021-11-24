70e7d2e7d5d6d8464990756befef5e130c54e541
Richard Marmorstein <richardm@stripe.com>
2021-11-24 01:36:15 -0500

    webhook handle

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,55 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import Stripe from "stripe";
+
+export const stripe = new Stripe(process.env.STRIPE_KEY, {
+  apiVersion: "2020-08-27",
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+async function buffer(readable) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks);
 }
+
+const webhookHandler = async (req, res) => {
+  if (req.method === "POST") {
+    console.log("Endpoint Stripe Key: ", process.env.STRIPE_KEY);
+
+    const buf = await buffer(req);
+
+    const sig = req.headers["stripe-signature"];
+    console.log("Signatures on endpoint: ", sig);
+
+    const webhookSecret = process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET;
+    console.log("Signing Secret on endpoint: ", webhookSecret);
+
+    let event;
+
+    try {
+      if (!sig || !webhookSecret) return;
+      event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+    } catch (err) {
+      console.log(`❌ Error message: ${err.message}`);
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    console.log(`Success!!`);
+
+    res.json({ received: true });
+  } else {
+    res.setHeader("Allow", "POST");
+
+    res.status(405).end("Method Not Allowed");
+  }
+};
+
+export default webhookHandler;
